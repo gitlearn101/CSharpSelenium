594ba6320a9074d74ab884c9530c5533ee7feb5f
gitlearn101 <anand.learn101@gmail.com>
2024-05-27 08:00:57 +0530

    sortwebtable.cs added

---
+++ LearnCSharpSelenium/SortWebtable.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace LearnCSharpSelenium
+{
+    class SortWebtable
+    {
+
+#pragma warning disable NUnit1032 // An IDisposable field/property should be Disposed in a TearDown method
+        public IWebDriver driver;
+#pragma warning restore NUnit1032 // An IDisposable field/property should be Disposed in a TearDown method
+
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            // Setup chromedriver using WebdriverManager
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+
+            // Implicit wait
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            driver.Manage().Window.Maximize();
+
+            driver.Url = "https://rahulshettyacademy.com/seleniumPractise/#/offers";
+
+        }
+
+
+
+    }
+}
